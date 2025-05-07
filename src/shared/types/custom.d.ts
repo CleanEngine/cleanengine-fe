520cf836fce19cca2443656982ac8e7ec12ab057
BHyeonKim <rlaqhguse@gmail.com>
2025-05-07 10:27:44 +0900

    config: vite-plugin-svgr 설치 및 svg 타입 추가

---
+++ src/shared/types/custom.d.ts
@@ -0,0 +1,7 @@
+declare module '*.svg' {
+	import type React from 'react';
+	import type { SVGProps } from 'react';
+	export const ReactComponent: React.FC<SVGProps<SVGSVGElement>>;
+	const src: string;
+	export default src;
+}
