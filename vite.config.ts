2b9314acccad1d174a12318fa3f8079df7cb9ae4
BHyeonKim <rlaqhguse@gmail.com>
2025-05-07 10:49:00 +0900

    config: vite-plugin-svgr => @svgr/rollup 으로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite';
+import svgr from '@svgr/rollup';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
+	plugins: [svgr(), tailwindcss(), reactRouter(), tsconfigPaths()],
 });
