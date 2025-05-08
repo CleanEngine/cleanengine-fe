fd75c906dae0c6442ab0daf13aa9691aeae253a1
BHyeonKim <rlaqhguse@gmail.com>
2025-05-08 14:05:33 +0900

    feat: 인풋컴포넌트 추가
    넘버 인풋 컴포넌트와 양(Quantity) 인풋 컴포넌트를 추가했습니다.
    
    issue: #1, #6

---
+++ src/entities/order/ui/QuantityInput/index.tsx
@@ -0,0 +1,39 @@
+import { IconMinus, IconPlus } from '~/assets/svgs';
+import NumberInput, { type NumberInputProps } from '~/shared/ui/NumberInput';
+
+export type PriceQuantityInputProps = {
+	onClickPlus: VoidFunction;
+	onClickMinus: VoidFunction;
+} & NumberInputProps;
+
+export default function PriceQuantityInput({
+	onClickPlus,
+	onClickMinus,
+	...props
+}: PriceQuantityInputProps) {
+	return (
+		<div className="relative">
+			<NumberInput
+				placeholder="주문금액"
+				step={1000}
+				min={0}
+				{...props}
+				style={{ paddingRight: '4rem' }}
+			/>
+			<button
+				type="button"
+				onClick={onClickPlus}
+				className="-translate-y-1/2 absolute top-1/2 right-1 w-7 cursor-pointer rounded-sm p-1.5 px-2 hover:bg-gray-200"
+			>
+				<IconPlus className="fill-gray-500" />
+			</button>
+			<button
+				type="button"
+				onClick={onClickMinus}
+				className="-translate-y-1/2 absolute top-1/2 right-8 w-7 cursor-pointer rounded-sm p-1.5 px-2 hover:bg-gray-200"
+			>
+				<IconMinus className="fill-gray-500" />
+			</button>
+		</div>
+	);
+}
---
+++ src/shared/ui/NumberInput/index.tsx
@@ -0,0 +1,13 @@
+import type { InputHTMLAttributes } from 'react';
+
+export type NumberInputProps = InputHTMLAttributes<HTMLInputElement>;
+
+export default function NumberInput(props: NumberInputProps) {
+	return (
+		<input
+			type="number"
+			className="input-number-hide h-8 w-full flex-2 rounded-md border-1 border-gray-200 px-2 text-gray-700 text-sm"
+			{...props}
+		/>
+	);
+}
