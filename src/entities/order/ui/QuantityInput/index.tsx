c2c89eee8156b9d12c7051c9c8a86a509ef3ea2d
BHyeonKim <rlaqhguse@gmail.com>
2025-05-08 17:24:04 +0900

    feat: 넘버 인풋 컴포넌트 disable시 스타일 추가

--- src/entities/order/ui/QuantityInput/index.tsx
+++ src/entities/order/ui/QuantityInput/index.tsx
@@ -23,14 +23,16 @@ export default function PriceQuantityInput({
 			<button
 				type="button"
 				onClick={onClickPlus}
-				className="-translate-y-1/2 absolute top-1/2 right-1 w-7 cursor-pointer rounded-sm p-1.5 px-2 hover:bg-gray-200"
+				className="-translate-y-1/2 absolute top-1/2 right-1 w-7 cursor-pointer rounded-sm p-1.5 px-2 hover:bg-gray-200 disabled:cursor-not-allowed"
+				disabled={props.disabled}
 			>
 				<IconPlus className="fill-gray-500" />
 			</button>
 			<button
 				type="button"
 				onClick={onClickMinus}
-				className="-translate-y-1/2 absolute top-1/2 right-8 w-7 cursor-pointer rounded-sm p-1.5 px-2 hover:bg-gray-200"
+				className="-translate-y-1/2 absolute top-1/2 right-8 w-7 cursor-pointer rounded-sm p-1.5 px-2 hover:bg-gray-200 disabled:cursor-not-allowed"
+				disabled={props.disabled}
 			>
 				<IconMinus className="fill-gray-500" />
 			</button>
--- src/shared/ui/NumberInput/index.tsx
+++ src/shared/ui/NumberInput/index.tsx
@@ -6,7 +6,7 @@ export default function NumberInput(props: NumberInputProps) {
 	return (
 		<input
 			type="number"
-			className="input-number-hide h-8 w-full flex-2 rounded-md border-1 border-gray-200 px-2 text-gray-700 text-sm"
+			className="input-number-hide h-8 w-full flex-2 rounded-md border-1 border-gray-200 px-2 text-gray-700 text-sm disabled:cursor-not-allowed disabled:bg-gray-200"
 			{...props}
 		/>
 	);
