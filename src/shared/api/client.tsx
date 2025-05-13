9a34630b2debac8cded1b8e358cb25d3baa235d0
BHyeonKim <rlaqhguse@gmail.com>
2025-05-13 11:14:49 +0900

    feat: api client 추가
    추후 사용하는 http client 라이브러리가 변경되도 해당 파일만 변경하면 되도록 ky를 클래스로 래핑하였습니다.
    
    issue: #1

---
+++ src/shared/api/client.tsx
@@ -0,0 +1,51 @@
+import ky, { type ResponsePromise } from 'ky';
+
+interface ApiClient {
+	get<T>(url: string, init?: RequestInit): ResponsePromise<T>;
+	post<T>(url: string, body?: BodyInit, init?: RequestInit): ResponsePromise<T>;
+	put<T>(url: string, body?: BodyInit, init?: RequestInit): ResponsePromise<T>;
+	delete<T>(url: string, init?: RequestInit): ResponsePromise<T>;
+	patch<T>(
+		url: string,
+		body?: BodyInit,
+		init?: RequestInit,
+	): ResponsePromise<T>;
+}
+
+class ApiClientImpl implements ApiClient {
+	private readonly client;
+
+	constructor() {
+		this.client = ky.create({ prefixUrl: import.meta.env.VITE_API_URL });
+	}
+
+	get<T>(url: string, init?: RequestInit): ResponsePromise<T> {
+		return this.client.get(url, init);
+	}
+
+	post<T>(
+		url: string,
+		body?: BodyInit,
+		init?: RequestInit,
+	): ResponsePromise<T> {
+		return this.client.post(url, { ...init, json: body });
+	}
+
+	put<T>(url: string, body?: BodyInit, init?: RequestInit): ResponsePromise<T> {
+		return this.client.put(url, { ...init, json: body });
+	}
+
+	delete<T>(url: string, init?: RequestInit): ResponsePromise<T> {
+		return this.client.delete(url, init);
+	}
+
+	patch<T>(
+		url: string,
+		body?: BodyInit,
+		init?: RequestInit,
+	): ResponsePromise<T> {
+		return this.client.patch(url, { ...init, json: body });
+	}
+}
+
+export default new ApiClientImpl();
