58e5223db0c97dd1060a2dad1972156e4bf8bb99
BHyeonKim <rlaqhguse@gmail.com>
2025-05-19 08:29:07 +0900

    feat: api 기본값 수정
    BTC => TRUMP 코인으로 수정하였습니다.

--- src/features/tradeview/api/tradeview.endpoints.ts
+++ src/features/tradeview/api/tradeview.endpoints.ts
@@ -2,7 +2,7 @@ import ApiClient from '~/shared/api/httpClient';
 import type { RowData } from '../types/tradeview.type';
 
 export default {
-	getPastData: async (ticker = 'BTC') => {
+	getPastData: async (ticker = 'TRUMP') => {
 		return await ApiClient.get<RowData[]>(`api/minute-ohlc?ticker=${ticker}`);
 	},
 };
