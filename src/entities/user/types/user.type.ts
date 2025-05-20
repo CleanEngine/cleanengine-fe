dc56e91d9024d00cba00c8fd3142a9849f38cacc
BHyeonKim <rlaqhguse@gmail.com>
2025-05-20 15:45:47 +0900

    feat: 주문 폼 매도 구현
    주문폼을 사용한 매도를 구현하였습니다.
    인풋컴포넌트에는 영어 알파벳 'E' 나 '-'가 들어가는 문제가 있었는데 이를 수정했습니다.
    가지고 있는 코인의 개수는 소수점 2번째 자리까지 나타냈습니다.
    
    issue: #11

--- src/entities/user/types/user.type.ts
+++ src/entities/user/types/user.type.ts
@@ -1,11 +1,21 @@
 import type { Response } from '~/shared/types/api';
 
+type Wallet = {
+	accountId: number;
+	buyPrice: string;
+	id: number;
+	roi: string;
+	size: string;
+	ticker: string;
+};
+
 type UserInfoResponseData = {
 	userId: number;
 	email: string;
 	nickname: string;
 	provider: string;
 	cash: number;
+	wallets: Wallet[];
 };
 
 export type UserInfoResponse = Response<UserInfoResponseData>;
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -9,3 +9,26 @@ export function isNegative(value: number): boolean {
 export function formatCurrencyKR(value: number): string {
 	return value.toLocaleString('ko-KR', { maximumFractionDigits: 2 });
 }
+
+export function filterNumber(value: string): boolean {
+	return /^[0-9]*(\.[0-9]*)?$/.test(value);
+}
+
+export function preventNonNumericInput(event: React.KeyboardEvent): void {
+	if (
+		!/^\d$/.test(event.key) &&
+		![
+			'Backspace',
+			'Tab',
+			'Enter',
+			'ArrowLeft',
+			'ArrowRight',
+			'Delete',
+			'Home',
+			'End',
+		].includes(event.key) &&
+		event.key !== '.'
+	) {
+		event.preventDefault();
+	}
+}
