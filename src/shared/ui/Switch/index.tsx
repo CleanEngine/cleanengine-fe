59ec7b132a518113cf19cee0cc600abed65f381f
BHyeonKim <rlaqhguse@gmail.com>
2025-05-20 15:43:10 +0900

    refactor: 스위치 컴포넌트 제네릭으로 변경
    스위치 컴포넌트를 하드코딩된 파라미터에서 제네릭으로 변경하였습니다.
    
    issue: #1

--- src/shared/ui/Switch/index.tsx
+++ src/shared/ui/Switch/index.tsx
@@ -1,31 +1,29 @@
 import clsx from 'clsx';
-import { type ButtonHTMLAttributes, type MouseEvent, useState } from 'react';
+import type { ButtonHTMLAttributes, MouseEvent } from 'react';
 
 type Value = ButtonHTMLAttributes<HTMLButtonElement>['value'];
 
-type SwitchProps<T extends Value> = {
-	value1: T;
-	value2: T;
+type SwitchProps<T extends Value, T1 extends T, T2 extends T> = {
+	value1: T1;
+	value2: T2;
 	text1: string;
 	text2: string;
-	onChange?: (value: T) => void;
+	selected: T1 | T2;
+	onChange?: (value: T1 | T2) => void;
 };
 
-export default function Switch<T extends Value>({
+export default function Switch<T extends Value, T1 extends T, T2 extends T>({
 	value1,
 	value2,
 	text1,
 	text2,
 	onChange,
-}: SwitchProps<T>) {
-	const [selected, setSelected] = useState<'left' | 'right'>('left');
-
+	selected,
+}: SwitchProps<T, T1, T2>) {
 	const handleSwitch = (e: MouseEvent<HTMLButtonElement>) => {
 		if (e.currentTarget.value === String(value1)) {
-			setSelected('left');
 			onChange?.(value1);
 		} else {
-			setSelected('right');
 			onChange?.(value2);
 		}
 	};
@@ -34,7 +32,7 @@ export default function Switch<T extends Value>({
 		<div className="relative flex h-8 select-none items-center rounded-md bg-gray-100 font-normal text-gray-600 text-sm">
 			<div
 				className={clsx(
-					selected === 'left' ? 'left-[25%]' : 'left-[75%]',
+					selected === value1 ? 'left-[25%]' : 'left-[75%]',
 					'-translate-x-1/2 absolute top-1 z-0 h-6 w-[48%] rounded-md bg-white shadow transition-all duration-200',
 				)}
 			/>
