02224af6cbb2c6d4cab0d75ac71ff6faec1b6707
BHyeonKim <rlaqhguse@gmail.com>
2025-05-23 14:13:05 +0900

    feat: 채팅창 열기버튼 컴포넌트 추가
    issue: #16

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as IconCoinBitcoin } from './coin-bitcoin.svg';
 export { ReactComponent as IconMagnifying } from './magnifying.svg';
 export { ReactComponent as IconPlus } from './plus-solid.svg';
 export { ReactComponent as IconMinus } from './minus-solid.svg';
+export { ReactComponent as IconHeadset } from './headset-solid.svg';
---
+++ src/features/chat/ui/ChatButton/index.tsx
@@ -0,0 +1,37 @@
+import { motion } from 'motion/react';
+
+import { IconHeadset } from '~/assets/svgs';
+
+type ChatButtonProps = {
+	isOpen: boolean;
+	handleClick: () => void;
+};
+
+const buttonVariant = {
+	closed: {
+		opacity: 1,
+	},
+	open: {
+		opacity: 0,
+		boxShadow: 'none',
+	},
+};
+
+export default function ChatButton({ isOpen, handleClick }: ChatButtonProps) {
+	return (
+		<motion.button
+			key="chat-button"
+			type="button"
+			className="absolute right-4 bottom-4 z-50 aspect-square w-12 cursor-pointer rounded-4xl bg-white p-2 shadow-sm"
+			onClick={handleClick}
+			initial={false}
+			whileHover={{ scale: 1.1 }}
+			whileTap={{ scale: 0.9 }}
+			animate={isOpen ? 'open' : 'closed'}
+			variants={buttonVariant}
+			exit={{ opacity: 0 }}
+		>
+			<IconHeadset />
+		</motion.button>
+	);
+}
