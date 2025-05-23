f9d012f058fb97e4475a4a4af99606576d7cfc61
BHyeonKim <rlaqhguse@gmail.com>
2025-05-23 14:21:15 +0900

    feat: AI 챗봇 구현
    issue: #16

---
+++ src/features/chat/index.ts
@@ -0,0 +1,2 @@
+export { default as AIChatBot } from './ui/AIChatBot';
+export { default as api } from './api/chat.endpoint';
---
+++ src/features/chat/ui/AIChatBot/index.tsx
@@ -0,0 +1,61 @@
+import { useMachine } from '@xstate/react';
+import { AnimatePresence } from 'motion/react';
+import { type ChangeEvent, type FormEvent, useState } from 'react';
+
+import useScrollToBottom from '~/shared/hooks/useScrollToBottom';
+import { chatMachine } from '../../model/chat.machine';
+import ChatButton from '../ChatButton';
+import ChatWindow from '../ChatWindow';
+import MessageBox from '../MessageBox';
+
+export default function AIChatBot() {
+	const [state, send] = useMachine(chatMachine);
+	const [isOpen, setIsOpen] = useState(false);
+	const messagesEndRef = useScrollToBottom([state.context.messageList]);
+
+	const handleOpenChatWindow = () => {
+		setIsOpen(true);
+	};
+
+	const handleCloseChatWindow = () => {
+		setIsOpen(false);
+	};
+
+	const handleQuestionFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+		send({ type: 'TYPING_QUESTION', question: e.target.value });
+	};
+
+	const handleSubmitQuestion = (e: FormEvent) => {
+		e.preventDefault();
+		send({ type: 'SUBMIT_EVENT' });
+	};
+
+	return (
+		<AnimatePresence mode="wait">
+			{isOpen ? (
+				<ChatWindow
+					handleClose={handleCloseChatWindow}
+					inputValue={state.context.question}
+					handleSubmit={handleSubmitQuestion}
+					handleInputValueChange={handleQuestionFieldChange}
+					state={state.context.state}
+					messageList={state.context.messageList}
+				>
+					{state.context.messageList.map((message, index) => {
+						const key = `msg-${index}-${message.isMine ? 'user' : 'ai'}`;
+						return (
+							<MessageBox
+								key={key}
+								direction={message.isMine ? 'right' : 'left'}
+								message={message.message}
+							/>
+						);
+					})}
+					<div ref={messagesEndRef} />
+				</ChatWindow>
+			) : (
+				<ChatButton handleClick={handleOpenChatWindow} isOpen={isOpen} />
+			)}
+		</AnimatePresence>
+	);
+}
