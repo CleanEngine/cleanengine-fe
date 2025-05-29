0f5c9411eb7a99a799bf4fa42b814f54653fac64
BHyeonKim <rlaqhguse@gmail.com>
2025-05-29 16:21:36 +0900

    test: useScrollToBottom 훅 단위테스트 추가
    issue: #19

--- src/shared/hooks/hooks.test.tsx
+++ src/shared/hooks/hooks.test.tsx
@@ -2,6 +2,7 @@ import { act, renderHook } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 
 import useClickOutside from './useClickOutside';
+import useScrollToBottom from './useScrollToBottom';
 
 describe('useClickOutside 훅 테스트', () => {
 	it('ref가 부착된 컴포넌트가 아닌 바깥 컴포넌트를 클릭하면 callback함수가 실행된다.', () => {
@@ -42,3 +43,72 @@ describe('useClickOutside 훅 테스트', () => {
 		expect(callback).not.toHaveBeenCalled();
 	});
 });
+
+describe('useScrollToBottom 훅 테스트', () => {
+	it('dependencies가 변경되면 scrollIntoView를 호출한다.', () => {
+		const mockScrollIntoView = vi.fn();
+
+		const { result, rerender } = renderHook(
+			({ deps }) => useScrollToBottom(deps),
+			{ initialProps: { deps: [1] } },
+		);
+
+		act(() => {
+			const element = document.createElement('div');
+			element.scrollIntoView = mockScrollIntoView;
+			result.current.current = element;
+		});
+
+		rerender({ deps: [2] });
+
+		expect(mockScrollIntoView).toHaveBeenCalled();
+	});
+});
+
+// FIXME: 테스트 환경에서 getBoundingClientRect를 mock으로 처리할 수 없음
+// describe('useDimensions 훅 테스트', () => {
+// 	it('참조하는 element의 width와 height가 반환된다.', () => {
+// 		const element = document.createElement('div');
+// 		const ref = { current: element };
+// 		vi.spyOn(element, 'getBoundingClientRect').mockImplementation(() => ({
+// 			width: 100,
+// 			height: 100,
+// 			x: 0,
+// 			y: 0,
+// 			top: 0,
+// 			left: 0,
+// 			right: 100,
+// 			bottom: 100,
+// 			toJSON: () => {},
+// 		}));
+
+// 		const { result } = renderHook(() => useDimensions(ref));
+
+// 		expect(result.current.width).toBe(100);
+// 		expect(result.current.height).toBe(100);
+// 	});
+
+// 	it('참조하는 element의 width와 height가 변경되면 변경된 값을 반환한다.', () => {
+// 		const element = document.createElement('div');
+// 		document.body.appendChild(element);
+// 		const ref = { current: element };
+
+// 		element.style.width = '100px';
+// 		element.style.height = '100px';
+// 		element.style.border = 'none';
+// 		element.style.padding = 'none';
+
+// 		const { result, rerender } = renderHook(() => useDimensions(ref));
+
+// 		expect(result.current.width).toBe(100);
+// 		expect(result.current.height).toBe(100);
+
+// 		element.style.height = '10px';
+// 		element.style.width = '10px';
+
+// 		rerender();
+
+// 		expect(result.current.height).toBe(10);
+// 		expect(result.current.width).toBe(10);
+// 	});
+// });
