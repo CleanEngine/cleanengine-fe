16fc1e0d806d7b3ba53a587f2fdba39921896ea7
BHyeonKim <rlaqhguse@gmail.com>
2025-06-05 17:46:44 +0900

    feat: 반응형 레이아웃 추가

--- src/app/routes/trade.$ticker.tsx
+++ src/app/routes/trade.$ticker.tsx
@@ -49,7 +49,7 @@ export default function TradeRouteComponent({
 	}));
 
 	return (
-		<div className="relative h-full bg-gray-100">
+		<div className="relative min-h-screen bg-gray-100 2xl:h-full">
 			<NavBar
 				to="/"
 				serviceName="IF"
@@ -60,14 +60,16 @@ export default function TradeRouteComponent({
 			{coinInfo && (
 				<CoinPriceWithName name={coinInfo?.name} ticker={coinInfo?.ticker} />
 			)}
-			<div className="grid h-[calc(100dvh-116px)] grid-cols-4 grid-rows-2 gap-4 p-4">
-				<div className="col-span-2 col-start-2 row-start-2">
+			<div className="grid h-[calc(100dvh-116px)] gap-4 p-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 2xl:grid-rows-2">
+				<div className="lg:col-span-full lg:row-span-1 lg:row-start-1 xl:col-span-full xl:row-span-1 xl:row-start-1 2xl:col-span-2 2xl:col-start-2 2xl:row-start-1">
 					<Container>
-						<ContainerTitle>실시간 체결 목록</ContainerTitle>
-						{coinInfo && <ExecutionList ticker={coinInfo.ticker} />}
+						<ContainerTitle>실시간 차트</ContainerTitle>
+						{coinInfo && (
+							<StockChart key={coinInfo.ticker} ticker={coinInfo.ticker} />
+						)}
 					</Container>
 				</div>
-				<div className="col-start-4 row-span-1 row-start-1">
+				<div className="lg:col-span-1 lg:col-start-2 lg:row-span-1 lg:row-start-2 xl:col-span-1 xl:col-start-3 xl:row-span-1 xl:row-start2 2xl:col-start-4 2xl:row-span-1 2xl:row-start-1">
 					<Container>
 						<ContainerTitle>주문 하기</ContainerTitle>
 						{isLoggedIn && coinInfo ? (
@@ -77,24 +79,22 @@ export default function TradeRouteComponent({
 						)}
 					</Container>
 				</div>
-				<div className="col-start-4 row-span-full row-start-2">
+				<div className="lg:col-span-1 lg:col-start-1 lg:row-span-1 lg:row-start-2 xl:col-span-1 xl:col-start-2 xl:row-span-1 xl:row-start-2 2xl:col-start-4 2xl:row-span-full 2xl:row-start-2">
 					<Container>
 						<ContainerTitle>실시간 호가</ContainerTitle>
 						{coinInfo && <Orderbook ticker={coinInfo.ticker} />}
 					</Container>
 				</div>
-				<div className="col-start-1 row-span-2 row-start-1">
+				<div className="lg:col-span-full lg:row-span-1 lg:row-start-3 xl:col-span-1 xl:col-start-1 xl:row-span-1 xl:row-start-2 2xl:col-span-2 2xl:col-start-2 2xl:row-start-2">
+					<Container>
+						<ContainerTitle>실시간 체결 목록</ContainerTitle>
+						{coinInfo && <ExecutionList ticker={coinInfo.ticker} />}
+					</Container>
+				</div>
+				<div className="lg:hidden xl:hidden 2xl:col-start-1 2xl:row-span-2 2xl:row-start-1">
 					<Container>
 						<ContainerTitle>가상화폐 리스트</ContainerTitle>
 						<CoinListWithSearchBar coinList={coinListWithIcon} />
-					</Container>
-				</div>
-				<div className="col-span-2 col-start-2 row-start-1">
-					<Container>
-						<ContainerTitle>실시간 차트</ContainerTitle>
-						{coinInfo && (
-							<StockChart key={coinInfo.ticker} ticker={coinInfo.ticker} />
-						)}
 					</Container>
 				</div>
 			</div>
