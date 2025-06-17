e19bcafe7c70d9e7f5e310ae0a395243cfa29692
BHyeonKim <rlaqhguse@gmail.com>
2025-06-17 13:03:13 +0900

    feat: 차트 기술지표 추가
    issues: #33

--- src/features/tradeview/ui/Chart/StockToolBar.tsx
+++ src/features/tradeview/ui/Chart/StockToolBar.tsx
@@ -1,56 +1,41 @@
 import * as am5stock from '@amcharts/amcharts5/stock';
 import { type PropsWithChildren, useEffect } from 'react';
-import type { MainPanel } from './MainPanel';
+import type { StockChart } from './StockChart';
 
-type StockToolBarProps = PropsWithChildren<Partial<MainPanel>>;
+type StockToolBarProps = PropsWithChildren<Partial<StockChart>>;
 
 export default function StockToolBar({
-	stockChartRef,
-	chartRootRef,
+	chartRoot,
+	stockChart,
 	chartToolbarContainerRef,
-	valueLegendRef,
 	children,
 }: StockToolBarProps) {
 	useEffect(() => {
-		if (
-			!chartRootRef ||
-			!chartRootRef.current ||
-			!stockChartRef ||
-			!stockChartRef.current ||
-			!chartToolbarContainerRef ||
-			!chartToolbarContainerRef.current ||
-			!valueLegendRef ||
-			!valueLegendRef.current
-		)
-			return;
-		const toolbar = am5stock.StockToolbar.new(chartRootRef.current, {
+		if (!chartRoot || !stockChart || !chartToolbarContainerRef?.current) return;
+
+		const toolbar = am5stock.StockToolbar.new(chartRoot, {
+			stockChart: stockChart,
 			container: chartToolbarContainerRef.current,
-			stockChart: stockChartRef.current,
 			controls: [
-				am5stock.IndicatorControl.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
-					legend: valueLegendRef.current,
+				am5stock.IndicatorControl.new(chartRoot, {
+					stockChart: stockChart,
 				}),
-				am5stock.DateRangeSelector.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
+				am5stock.DateRangeSelector.new(chartRoot, {
+					stockChart: stockChart,
 				}),
-				am5stock.PeriodSelector.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
+				am5stock.DrawingControl.new(chartRoot, {
+					stockChart: stockChart,
 				}),
-				am5stock.SeriesTypeControl.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
+				am5stock.ResetControl.new(chartRoot, {
+					stockChart: stockChart,
 				}),
-				am5stock.DrawingControl.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
+				am5stock.DataSaveControl.new(chartRoot, {
+					stockChart: stockChart,
+					storageId: 'invest-future-chart',
+					autoSave: true,
 				}),
-				am5stock.DataSaveControl.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
-				}),
-				am5stock.ResetControl.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
-				}),
-				am5stock.SettingsControl.new(chartRootRef.current, {
-					stockChart: stockChartRef.current,
+				am5stock.SettingsControl.new(chartRoot, {
+					stockChart: stockChart,
 				}),
 			],
 		});
@@ -58,7 +43,7 @@ export default function StockToolBar({
 		return () => {
 			toolbar.dispose();
 		};
-	}, [stockChartRef, chartRootRef, chartToolbarContainerRef, valueLegendRef]);
+	}, [chartRoot, stockChart, chartToolbarContainerRef]);
 
 	return children;
 }
