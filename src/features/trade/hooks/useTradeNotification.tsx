f4a24f00d7a35ba528f8eabdae784028c065b6b0
BHyeonKim <rlaqhguse@gmail.com>
2025-06-25 05:14:18 +0900

    fix: 서버 구현 변경에 따른 토스트 메시지 형식 수정

--- src/features/trade/hooks/useTradeNotification.tsx
+++ src/features/trade/hooks/useTradeNotification.tsx
@@ -5,10 +5,8 @@ import { useStompClient } from '~/app/provider/StompProvider';
 
 type TradeNotification = {
 	ticker: string;
-	price: number;
 	size: number;
 	type: 'ask' | 'bid';
-	tradedTime: string;
 };
 
 export default function useTradeNotification(userId: number) {
@@ -22,7 +20,7 @@ export default function useTradeNotification(userId: number) {
 			(message) => {
 				const parsedData = JSON.parse(message.body) as TradeNotification;
 				const tradeType = parsedData.type === 'ask' ? '매도' : '매수';
-				const toastMessage = `${parsedData.ticker} ${tradeType} 체결 완료 - 가격: ${parsedData.price}, 수량: ${parsedData.size}`;
+				const toastMessage = `${parsedData.ticker} ${tradeType} 체결 완료 - 수량: ${parsedData.size}`;
 				toast.success(toastMessage);
 			},
 		);
