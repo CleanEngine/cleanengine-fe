aa0f69b4afebcf7db6fb69e7bd624c5ff07ba49f
BHyeonKim <rlaqhguse@gmail.com>
2025-07-14 22:51:17 +0900

    feat: profile route 추가

--- src/app/routes/profile.tsx
+++ src/app/routes/profile.tsx
@@ -1,5 +1,15 @@
+import { api } from '~/entities/user';
 import { ProfileModal } from '~/widgets/user';
+import type { Route } from './+types/profile';
 
-export default function ProfileRouteComponent() {
-	return <ProfileModal />;
+export async function loader() {
+	const response = await api.getUserInfo();
+	const { data } = await response.json();
+	return data;
+}
+
+export default function ProfileRouteComponent({
+	loaderData,
+}: Route.ComponentProps) {
+	return <ProfileModal userInfo={loaderData} />;
 }
