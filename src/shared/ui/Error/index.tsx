9ed26f2276de4d307b87ed4cea0d280473e6a840
BHyeonKim <rlaqhguse@gmail.com>
2025-07-21 23:54:21 +0900

    feat: Error 컴포넌트 추가

---
+++ src/shared/ui/Error/index.tsx
@@ -0,0 +1,20 @@
+import Lottie from 'lottie-react';
+import ErrorAnimation from '~/assets/lotties/error.json';
+
+type ErrorComponentProps = {
+	title: string;
+	description: string;
+};
+
+export default function ErrorComponent({
+	title,
+	description,
+}: ErrorComponentProps) {
+	return (
+		<div className="flex h-full w-full flex-col items-center justify-center">
+			<Lottie animationData={ErrorAnimation} className="w-40" loop autoplay />
+			<p className="font-semibold text-2xl text-gray-700">{title}</p>
+			<p className="pt-1 text-gray-500">{description}</p>
+		</div>
+	);
+}
