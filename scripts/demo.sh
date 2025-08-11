0caa51153f0f3b4f0d35e66044c2648d7c6edd58
BHyeonKim <rlaqhguse@gmail.com>
2025-08-11 17:52:05 +0900

    feat: 데모 스크립트 수정
    기존 빌드폴더가 있으면 해당 폴더를 삭제하고 새로 빌드하도록
    수정하였습니다.

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -4,8 +4,10 @@ export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 chmod +x "${SCRIPT_DIR}/backend.sh"
 
+# 백엔드 빌드 및 실행
 "${SCRIPT_DIR}/backend.sh"
 
+# 3000번 포트가 사용중인지 확인
 if lsof -i :3000 > /dev/null 2>&1; then
   echo "3000번 포트가 사용 중입니다. 해당 프로세스를 종료하시겠습니까? (y/n)"
   read -r answer
@@ -18,4 +20,17 @@ if lsof -i :3000 > /dev/null 2>&1; then
   fi
 fi
 
-yarn start
+# 프로젝트 폴더로 이동
+cd "${SCRIPT_DIR}/.."
+
+git checkout main
+
+git pull origin main
+
+# 기존 빌드파일 삭제
+if [ -d "build" ]; then
+  echo "build 폴더가 있습니다. 해당 폴더를 삭제하겠습니다."
+  rm -rf build
+fi
+
+yarn build && yarn start
